b8aefd8b097c9a5cef357871c2a758048e4f8c4e
OlivierBaulier <olivier.baulier@oseline.com>
2022-06-26 11:03:38 +0200

    Test new core Vs legacy core

---
+++ controller/src/test/java/com/example/demo/controller/CoreLegacyVsNewTest.java
@@ -0,0 +1,207 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.in.ShoeFilter;
+import com.example.demo.dto.out.Shoe;
+import com.example.demo.dto.out.Shoes;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.math.BigInteger;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@DisplayName("Test legacy vs new core")
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CoreLegacyVsNewTest {
+
+
+        private RestTemplate restTemplate;
+
+        private  Shoes newCoreDefaultShoes = Shoes.builder()
+                .shoes(List.of(Shoe.builder()
+                        .name("New shoe")
+                        .color(ShoeFilter.Color.BLUE)
+                        .size(BigInteger.ONE)
+                        .build()))
+                .build();
+
+        @LocalServerPort
+        int randomServerPort;
+
+
+        @Before
+        public void initLog(){
+
+            ClientHttpRequestFactory factory =
+                    new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory());
+            this.restTemplate = new RestTemplate(factory);
+
+            List<ClientHttpRequestInterceptor> interceptors = restTemplate.getInterceptors();
+            if (CollectionUtils.isEmpty(interceptors)) {
+                interceptors = new ArrayList<>();
+            }
+            interceptors.add(new LoggingInterceptor());
+            restTemplate.setInterceptors(interceptors);
+        }
+
+        @DisplayName("Test: legacy on '/shoes/search'")
+        @Test
+        public void testLegacyCore() throws URISyntaxException
+        {
+            final String url = "http://localhost:"+randomServerPort+"/shoes/search";
+            Shoes expectedShoes = Shoes.builder()
+                    .shoes(List.of(Shoe.builder()
+                            .name("Legacy shoe")
+                            .color(ShoeFilter.Color.BLUE)
+                            .size(BigInteger.ONE)
+                            .build()))
+                    .build();
+
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("version", "1");
+
+            HttpEntity request = new HttpEntity(headers);
+
+            // make an HTTP GET request with headers
+            ResponseEntity<Shoes> result = restTemplate.exchange(
+                    url,
+                    HttpMethod.GET,
+                    request,
+                    Shoes.class
+            );
+
+            //Verify http code
+            Assert.assertEquals("Http code",200,result.getStatusCode().value());
+            //Verify shoes
+            Assert.assertEquals("Legacy shoes",expectedShoes, result.getBody());
+        }
+
+    @DisplayName("Test: get on new core the default shoe")
+    @Test
+    public void testNewCoreDefault() throws URISyntaxException
+    {
+        final String url = "http://localhost:"+randomServerPort+"/shoes/search";
+        Shoes expectedShoes = Shoes.builder()
+                .shoes(List.of(Shoe.builder()
+                    .name("New shoe")
+                    .color(ShoeFilter.Color.BLACK)
+                    .size(BigInteger.TWO)
+                    .build()))
+               .build();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("version", "2");
+
+        HttpEntity request = new HttpEntity(headers);
+
+        // make an HTTP GET request with headers
+        ResponseEntity<Shoes> result = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                request,
+                Shoes.class
+        );
+
+        //Verify http code
+        Assert.assertEquals("Http code",200,result.getStatusCode().value());
+        //Verify shoes
+        Assert.assertEquals("Legacy shoes",expectedShoes, result.getBody());
+    }
+
+    @DisplayName("Test: get on new core the filtered shoe")
+    @Test
+    public void testNewCoreWithFilter() throws URISyntaxException {
+        final String url = "http://localhost:" + randomServerPort + "/shoes/search";
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(url)
+                .queryParam("size", BigInteger.ONE)
+                .queryParam("color", ShoeFilter.Color.BLUE);
+        String uriBuilder = builder.build().encode().toUriString();
+
+
+        Shoes expectedShoes = Shoes.builder()
+                .shoes(List.of(Shoe.builder()
+                        .name("New shoe")
+                        .color(ShoeFilter.Color.BLUE)
+                        .size(BigInteger.ONE)
+                        .build()))
+                .build();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("version", "2");
+
+        HttpEntity request = new HttpEntity(headers);
+
+        // make an HTTP GET request with headers
+        ResponseEntity<Shoes> result = restTemplate.exchange(
+                uriBuilder,
+                HttpMethod.GET,
+                request,
+                Shoes.class
+        );
+
+        //Verify http code
+        Assert.assertEquals("Http code",200,result.getStatusCode().value());
+        //Verify shoes
+        Assert.assertEquals("Legacy shoes",expectedShoes, result.getBody());
+    }
+
+    @DisplayName("Test: get on new core the filtered shoe with a bad argument")
+    @Test
+    public void testNewCoreWithDabFilter() throws URISyntaxException
+    {
+        final String url = "http://localhost:"+randomServerPort+"/shoes/search";
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(url)
+                .queryParam("size", BigInteger.ONE)
+                .queryParam("BadColor", ShoeFilter.Color.BLUE);
+        String uriBuilder = builder.build().encode().toUriString();
+
+        // with bad parameters expected shoes will be the default
+        Shoes expectedShoes = Shoes.builder()
+                .shoes(List.of(Shoe.builder()
+                        .name("New shoe")
+                        // Due to bad parameter the color stays in default color (black)
+                        .color(ShoeFilter.Color.BLACK)
+                        .size(BigInteger.ONE)
+                        .build()))
+                .build();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("version", "2");
+
+        HttpEntity request = new HttpEntity(headers);
+
+        // make an HTTP GET request with headers
+        ResponseEntity<Shoes> result = restTemplate.exchange(
+                uriBuilder,
+                HttpMethod.GET,
+                request,
+                Shoes.class
+        );
+
+        //Verify http code
+        Assert.assertEquals("Http code",200,result.getStatusCode().value());
+        //Verify shoes
+        Assert.assertEquals("Legacy shoes",expectedShoes, result.getBody());
+    }
+}
---
+++ controller/src/test/java/com/example/demo/controller/LoggingInterceptor.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpResponse;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+
+public class LoggingInterceptor implements org.springframework.http.client.ClientHttpRequestInterceptor {
+
+    static Logger LOGGER =  LoggerFactory.getLogger(LoggingInterceptor.class);
+
+    @Override
+    public ClientHttpResponse intercept(
+            HttpRequest req, byte[] reqBody, ClientHttpRequestExecution ex) throws IOException {
+        LOGGER.info("Request {}({}) body: {}", req.getMethod(), req.getURI(), new String(reqBody, StandardCharsets.UTF_8));
+        ClientHttpResponse response = ex.execute(req, reqBody);
+        InputStreamReader isr = new InputStreamReader(
+                response.getBody(), StandardCharsets.UTF_8);
+        String body = new BufferedReader(isr).lines()
+                .collect(Collectors.joining("\n"));
+        LOGGER.info("Response body: {}", body);
+        return response;
+    }
+}
